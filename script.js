e5f13ec527392ad0ddbb63b6f374fbbde708497b
Vineet Singh <119122020+vineet2singh@users.noreply.github.com>
2023-09-13 18:33:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+console.log("hello")
+
+{/* <script> */}
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+//---------- Text  Animation-------
+
+const txtFull = "CodingWave"; //the text goes here
+const txtArr = txtFull.split("");
+
+function getRandomTime() {
+  return Math.random() * 0.5;
+}
+function getEle(id) {
+  return document.getElementById("text");
+}
+function ModifyTxt(el, txt) {
+  return (el.innerHTML = txt);
+}
+function writeOut(txtArr, curChar = 0) {
+  const randomTime = getRandomTime();
+  setTimeout(
+    () => {
+      const el = getEle("text");
+      let elTxt = el.innerHTML;
+      elTxt += txtArr[curChar];
+      ModifyTxt(el, elTxt);
+      //check if next iteration is out of range
+      if (curChar + 1 == txtArr.length) {
+        return deleteOut(txtArr, txtArr.length);
+      }
+      return writeOut(txtArr, curChar + 1);
+    },
+
+    randomTime * 1000
+  );
+}
+function deleteOut(txtArr, curChar = 0) {
+  const randomTime = getRandomTime();
+  setTimeout(
+    () => {
+      const el = getEle("text");
+      let elTxt = el.innerHTML;
+      //remove
+      const elTxtArr = elTxt.split("");
+      const elTxtArrLen = elTxtArr.length;
+      console.log(elTxtArr.length - 1);
+      const newElTxt = elTxtArr.splice(0, curChar - 1).join("");
+      ModifyTxt(el, newElTxt);
+      if (elTxtArrLen == 0) {
+        return writeOut(txtArr, 0);
+      }
+      return deleteOut(txtArr, elTxtArrLen - 1);
+    },
+
+    randomTime * 1000
+  );
+  return;
+}
+writeOut(txtArr);
